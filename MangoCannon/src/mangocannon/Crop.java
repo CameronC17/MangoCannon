0dac50c13a8e1bb5a3769d256bc94eab5d5956f5
Tom <itomhanson@gmail.com>
2016-01-25 15:04:24 +0000

    Crop.java updated

--- MangoCannon/src/mangocannon/Crop.java
+++ MangoCannon/src/mangocannon/Crop.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mangocannon;
+
+import java.util.Date;
 
 /**
  *
- * @author jacobburns
+ * @author tomhanson
  */
 public class Crop {
     
+    private String cropType;
+    private long uniqueId;
+    private float estimatedYield;
+    private Date plantDate;
+    private Date expectedHarvest;
+    
+    String getCropType(){
+        return cropType;
+    }
+    void setCropType(String theCropType){
+        this.cropType = cropType;
+    }
+    
+    long getUniqueId(){
+        return uniqueId;
+    }
+    void setUniqueId(long theUniqueId){
+        this.uniqueId = uniqueId;
+    }
+    
+    float getEstimatedYield(){
+        return estimatedYield;
+    }
+    void setEstimatedYield(float theEstimatedYield){
+        this.estimatedYield = theEstimatedYield;
+    }
+    
+    Date getPlantDate(){
+        return plantDate;
+    }
+    void setPlantDate(Date thePlantDate){
+        this.plantDate = thePlantDate;
+    }
+    
+    Date getExpectedHarvest(){
+        return expectedHarvest;
+    }
+    void setExpectedHarvest(Date theExpectedHarvest){
+        this.expectedHarvest = theExpectedHarvest;
+    }
 }
