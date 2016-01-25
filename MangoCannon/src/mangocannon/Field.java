80dec5393274b109f6e8967c44c3f6c1a298816e
Jacob <jacobburns@live.co.uk>
2016-01-25 14:18:22 +0000

    Blank classes added

---
+++ MangoCannon/src/mangocannon/Field.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangocannon;
+
+/**
+ *
+ * @author jacobburns
+ */
+public class Field {
+    private String fieldName;
+    private long uniqueId;
+    private GPSBoudary gpsBoundary;
+    private FieldStation fieldStation;
+    private Crop crop;
+}
---
+++ MangoCannon/src/mangocannon/UserRegistration.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangocannon;
+
+/**
+ *
+ * @author jacobburns
+ */
+public class UserRegistration {
+    private User[] users;
+    
+}
