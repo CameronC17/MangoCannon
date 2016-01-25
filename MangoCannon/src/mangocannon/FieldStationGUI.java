7f178526014a9262158311ebfc05a946f78ecd5a
CameronC17 <cameronchalmers@homail.co.uk>
2016-01-25 14:42:37 +0000

    Update FieldStationGUI.java

--- MangoCannon/src/mangocannon/FieldStationGUI.java
+++ MangoCannon/src/mangocannon/FieldStationGUI.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,5 +10,11 @@ package mangocannon;
  * @author jacobburns
  */
 public class FieldStationGUI {
+    //Private variable initialisers
+    private GUISettings guiSettings;
+    
+    public void connectToNetwork(){
+        //Connect to what network?! lolz
+    }
     
 }
