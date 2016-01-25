427df973302bef7fea3b82b22438c81828823641
Jacob <jacobburns@live.co.uk>
2016-01-25 15:04:21 +0000

    Updated FieldStation to match class diagram

--- MangoCannon/src/mangocannon/FieldStation.java
+++ MangoCannon/src/mangocannon/FieldStation.java
@@ -10,5 +10,49 @@ package mangocannon;
  * @author jacobburns
  */
 public class FieldStation {
+    private Sensor sensorList[];
+    private String name;
+    private long uniqueId;
+    private boolean fixed;
+    private GPS gps;
     
+    Sensor getSensor(){
+        return sensorList[];
+    }
+    
+    void setSensor(Sensor theSensorList[]){
+        this.sensorList = this.sensorList + theSensorList;
+    }
+    
+    String getName(){
+        return name;
+    }
+    
+    void setName(String theName){
+        this.name = theName;
+    }
+    
+    long getUniqueId(){
+        return uniqueId;
+    }
+    
+    void setUniqueId(long theUniqueId){
+        this.uniqueId = theUniqueId;
+    }
+    
+    boolean getFixed(){
+        return fixed;
+    }
+    
+    void setFixed(boolean theFixed){
+        this.fixed = theFixed;
+    }
+    
+    GPS getGPS(){
+        return gps;
+    }
+    
+    void setGPS(GPS theGps){
+        this.gps = theGps;
+    }
 }
